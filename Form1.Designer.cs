f37c60a7b532eae89b046a0de0f4ab1dd0642622
Jean-Michel <jeanmialbert10@hotmail.com>
2024-09-26 12:42:34 -0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace HelloGitHubVS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.helloButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // helloButton
+            // 
+            this.helloButton.Location = new System.Drawing.Point(124, 102);
+            this.helloButton.Name = "helloButton";
+            this.helloButton.Size = new System.Drawing.Size(253, 73);
+            this.helloButton.TabIndex = 0;
+            this.helloButton.Text = "Hello GitHub";
+            this.helloButton.UseVisualStyleBackColor = true;
+            this.helloButton.Click += new System.EventHandler(this.helloButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(960, 532);
+            this.Controls.Add(this.helloButton);
+            this.Name = "Form1";
+            this.Text = "Github et Git pour Visual Studio";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button helloButton;
+    }
+}
+
